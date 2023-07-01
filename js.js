6052e5508cb9fffae474b09690f81d2a3c9c181f
Jim Moroco <jim.moroco@gmail.com>
2023-07-01 03:18:26 -0500

    chore: initial commit

---
+++ js.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    const header = document.querySelector("#header");
+    fetch("/header.html")
+        .then(res => res.text())
+        .then(data => {
+            header.innerHTML = data;
+        });
+    const footer = document.querySelector("#footer");
+    fetch("/footer.html")
+        .then(res => res.text())
+        .then(data => {
+            footer.innerHTML = data;
+        })
+}
