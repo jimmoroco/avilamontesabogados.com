b83b6056467a29de2ea344c486f821b4540a3f48
Jim Moroco <jim.moroco@gmail.com>
2023-07-04 21:20:57 -0500

    feat: add new pages and refactoring

--- js/js.js
+++ js/js.js
@@ -1,14 +1,17 @@
 window.onload = function () {
     const header = document.querySelector("#header");
-    fetch("/header.html")
+    fetch("/templates/header.html")
         .then(res => res.text())
         .then(data => {
             header.innerHTML = data;
         });
     const footer = document.querySelector("#footer");
-    fetch("/footer.html")
+    fetch("/templates/footer.html")
         .then(res => res.text())
         .then(data => {
             footer.innerHTML = data;
         })
+}
+function showMenu(element, isVisible) {
+    document.getElementById(element).style.display = isVisible ? "block" : "none";
 }
